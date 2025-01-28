72ee94806e8fa31b026af06caca05c42b8b974cc
Gautam Panchal <gautampanchal@Gautams-MacBook-Pro.local>
2025-01-28 12:44:14 +0530

    changed to car data

--- server.js
+++ server.js
@@ -1,98 +1,119 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-const userData = require("./MOCK_DATA.json");
+const carData = require("./MOCK_DATA.json");  // Mock data file containing car details
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLSchema, GraphQLList, GraphQLID, GraphQLInt, GraphQLString } = graphql;
 const { graphqlHTTP } = require("express-graphql");
 
-const UserType = new GraphQLObjectType({
-  name: "User",
+// Define the Car Type
+const CarType = new GraphQLObjectType({
+  name: "Car",
   fields: () => ({
     id: { type: GraphQLInt },
-    firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString },
-    email: { type: GraphQLString },
-    password: { type: GraphQLString },
+    make: { type: GraphQLString },
+    model: { type: GraphQLString },
+    year: { type: GraphQLInt },
+    color: { type: GraphQLString },
+    price: { type: GraphQLInt },
   }),
 });
 
+// Define the Root Query for fetching car data
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    getAllUsers: {
-      type: new GraphQLList(UserType),
+    getAllCars: {
+      type: new GraphQLList(CarType),
       args: { id: { type: GraphQLInt } },
       resolve(parent, args) {
-        // If an id is passed in args, return that specific user
+        // If an id is passed in args, return that specific car
         if (args.id) {
-          return userData.filter((user) => user.id === args.id);
+          return carData.filter((car) => car.id === args.id);
         }
-        return userData; // return all users if no id is provided
+        return carData; // return all cars if no id is provided
       },
     },
-    findUserById: {
-      type: UserType,
-      description: "Fetch single user by ID",
+    findCarById: {
+      type: CarType,
+      description: "Fetch single car by ID",
       args: { id: { type: GraphQLInt } },
       resolve(parent, args) {
-        return userData.find((a) => a.id === args.id);
+        return carData.find((car) => car.id === args.id);
       },
     },
-    findUserByEmail: {
-      type: UserType,
-      description: "Fetch single user by email",
-      args: { email: { type: GraphQLString } },
+    findCarByMake: {
+      type: new GraphQLList(CarType),
+      description: "Fetch cars by make",
+      args: { make: { type: GraphQLString } },
       resolve(parent, args) {
-        return userData.find((a) => a.email === args.email);
+        return carData.filter((car) => car.make === args.make);
+      },
+    },
+    findCarByModel: {
+      type: CarType,
+      description: "Fetch single car by model",
+      args: { model: { type: GraphQLString } },
+      resolve(parent, args) {
+        return carData.find((car) => car.model === args.model);
       },
     },
   },
 });
 
+// Define Mutation (for example, adding a new car)
 const Mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-    createUser: {
-      type: UserType,
+    createCar: {
+      type: CarType,
       args: {
-        firstName: { type: GraphQLString },
-        lastName: { type: GraphQLString },
-        email: { type: GraphQLString },
-        password: { type: GraphQLString },
+        make: { type: GraphQLString },
+        model: { type: GraphQLString },
+        year: { type: GraphQLInt },
+        color: { type: GraphQLString },
+        price: { type: GraphQLInt },
       },
       resolve(parent, args) {
-        userData.push({
-          id: userData.length + 1,
-          firstName: args.firstName,
-          lastName: args.lastName,
-          email: args.email,
-          password: args.password,
-        });
-        return args;
+        // Simulate adding a car to the database (mock)
+        const newCar = {
+          id: carData.length + 1,
+          make: args.make,
+          model: args.model,
+          year: args.year,
+          color: args.color,
+          price: args.price,
+        };
+        carData.push(newCar);  // Adding to mock data
+        return newCar;
       },
     },
   },
 });
 
+// Create the GraphQL Schema
 const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
+// Set up GraphQL HTTP endpoint
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: true,
+    graphiql: true, // Enable GraphiQL interface for testing queries
   })
 );
 
-app.get("/rest/getAllUsers", (req, res) => {
-  res.send(userData);
+// Set up REST API endpoint to fetch all cars
+app.get("/rest/getAllCars", (req, res) => {
+  res.send(carData);  // Return the mock car data
 });
 
-app.get("/", (req, res)=>{
-    res.sendFile(page.html);
-})
+// Set up a simple route (could be your homepage or static page)
+app.get("/", (req, res) => {
+  res.send("Welcome to the Car Inventory API");
+});
 
+// Start the server on the defined port
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
